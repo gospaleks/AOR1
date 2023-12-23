1341b79fc6bfbc7b00d23af2ec7e8656607f63b4
Aleksandar Gospavić <151999326+gospaleks@users.noreply.github.com>
2023-12-23 02:23:31 +0100

    2020-II-kol

--- Blanketi/2020-II-kol-zad6.asm
+++ Blanketi/2020-II-kol-zad6.asm
@@ -7,7 +7,7 @@
 	mat dw 1,2,3,4,5
 		dw 6,7,8,9,10
 		dw 11,12,13,14,15
-		dw 16,17,18,19,20	; u rezultatu: 34, 38, 42, 46, 50
+		dw 16,17,18,19,20		; u rezultatu: 34, 38, 42, 46, 50 (dekadno)
 .code
 sumaSvakogKtog proc
 	push ebp
@@ -18,23 +18,23 @@ sumaSvakogKtog proc
 	mov eax, [ebp+12]
 	mov ecx, [ebp+8]
 	div cl
-	movzx ecx, al			; broj prolaska
+	movzx ecx, al				; broj prolaska
 	
-	mov edx, [ebp+8]		; korak
+	mov edx, [ebp+8]			; korak
 	shl edx, 1				; korak*2 jer je 16b niz
 
 	xor esi, esi
-	xor ebx, ebx			; suma = 0
+	xor ebx, ebx				; suma = 0
 	mov edi, [ebp+16]
 	petlja:
 		
 		movzx eax, word ptr [edi][esi]
-		add ebx, eax		; suma += eax
+		add ebx, eax			; suma += eax
 
 		add esi, edx	
 		loop petlja
 
-	mov [ebp+16], ebx		; smesti sumu na dno steka
+	mov [ebp+16], ebx			; smesti sumu na dno steka
 
 	popad
 	popfd
@@ -49,7 +49,7 @@ main proc
 	mul m
 	shl eax, 1				; 2 * (n-1) * m
 	lea edi, mat
-	add edi, eax			; adresa prvog elementa u poslednjoj vrsti
+	add edi, eax				; adresa prvog elementa u poslednjoj vrsti
 
 	movzx ecx, m
 	mov ebx, ecx
@@ -67,7 +67,7 @@ main proc
 		call sumaSvakogKtog
 		pop eax				; u EAX je ono sto treba se upise u poslednju vrstu
 
-		mov [edi], ax		; smesti u poslednju vrstu (ax jer su 16b svakako)
+		mov [edi], ax			; smesti u poslednju vrstu (ax jer su 16b svakako)
 		add edi, 2			; predji na sledeci element u poslednjoj vrsti
 
 		add esi, 2			; predji na sledeci pocetni element u prvoj vrsti
