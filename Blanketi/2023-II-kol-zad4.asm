217b7d0d4e5b3b99a5097d3dd26a4475e675e3fd
Aleksandar Gospavić <151999326+gospaleks@users.noreply.github.com>
2023-12-31 12:42:16 +0100

    Create 2023-II-kol-zad4.asm

---
+++ Blanketi/2023-II-kol-zad4.asm
@@ -0,0 +1,49 @@
+.386
+.model flat,c
+.stack 4096
+.data
+	mat dw 5, 3, 1
+		dw 3, 4, 5
+		dw 1, 4, 7
+		dw 4, 8, 10
+	n dw 4
+	m dw 3
+	rez dd ?			; ocekivan rez je 5
+.code
+main proc
+	
+	; 2022/23 II kol zad 4.
+
+	; da dobijem drugi element u pretposlednjoj vrsti
+	; ebx je (2*m * (n-2)) i dodam 2 jer je druga kolona
+	movzx ebx, m
+	shl ebx, 1
+	movzx eax, n
+	sub eax, 2
+	mul ebx
+
+	movzx edi, word ptr mat[eax][2]		; u EDI element sa kojim poredim
+
+	; sada matricu mogu da posmatram kao niz
+	movzx eax, n
+	movzx ecx, m
+	mul ecx
+	mov ecx, eax		; u ECX broj proslaska (n*m)
+
+	xor edx, edx		; u EDX je rez koji posle smestam u memoriju
+	xor esi, esi
+	petlja:
+		movzx eax, word ptr mat[esi]
+		cmp eax, edi
+		jle dalje
+		inc edx
+		dalje:
+		add esi, 2
+		loop petlja
+
+	mov rez, edx
+
+	nop
+	ret
+main endp
+end main
