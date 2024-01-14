ac9e71ff68c5bab2ddd3639e8325912ce40e7ef4
Aleksandar Gospavić <151999326+gospaleks@users.noreply.github.com>
2024-01-15 00:13:35 +0100

    Ispravka 2022-II-kol-zad5 u C fajlu

--- Blanketi/2022-II-kol-zad5/Main.c
+++ Blanketi/2022-II-kol-zad5/Main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-extern void funkcija(char n, int a[], int x, int y, int z);
+extern void funkcija(char n, int a[], int *x, int *y, int *z);
 
 /*
 	PROBLEM:
@@ -17,6 +17,8 @@ int main() {
 		scanf_s("%d", &a[i]);
 
 	int A = 1;
+	// * ovo oliver nesto reko kao da ne cast-ujemo u char jer stek prima samo 32b i zavisi od kompajlera
+	//   dal ce esp da skoci kako treba, al u ovom slucaju radi, tkd bukv najnebitnija stvar u ovaj zad
 	funkcija((char)n, a, &a[0], &a[n - 1], &A);
 	
 	for (int i = 0; i < n; ++i)
